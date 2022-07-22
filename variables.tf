d86c0d8edeb75144d561fa53db9e1881b0b35d93
tulasi574 <83654898+tulasi574@users.noreply.github.com>
2022-07-22 12:45:18 +0530

    Update variables.tf
    updated security group name

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "instance_type" {
 
 variable "security_group" {
   description = "Name of security group"
-  default     = "myterraformsg"
+  default     = "myterraformsg123"
 }
 
 variable "tag_name" {
